537ed8633d2760e27eb08ac6e90ed7698857015d
birdlin <104408390+xplin13@users.noreply.github.com>
2023-06-30 11:36:10 +0800

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,27 @@
+# *Two-level Consistency Metric for Infrared and Visible Image Fusion*
+
+## Platform
+python 3.7.0\
+torch 1.4.0\
+torchvision 0.5.0
+
+## Fusion framework
+MSSD is utilized to train our network. The dataset can be downloaded at https://drive.google.com/drive/folders/1QE3xytmOJ-zNGUobJyFP_J0JatxszjR3
+![图片](./demonstrate_images/network.png)
+
+Our trained model can be downloaded at: https://pan.baidu.com/s/14wEtKrzXypk8ReVg6meXLg?pwd=8jny .\
+The model should be put into checkpoint folder for test.
+
+## Update
+In a subsequent study, we found that combine the two types of high-frequency attention source images in the computation of the weight map could better preserve the  saliency of the infrared target in the fusion results and obtain better visual quality. Therefore, we update the loss function named wse_update here, and if you use this loss function, please cite the article:\
+@article{lin2022two,\
+  title={Two-Level Consistency Metric for Infrared and Visible Image Fusion},\
+  author={Lin, Xiaopeng and Zhou, Guanxing and Tu, Xiaotong and Huang, Yue and Ding, Xinghao},\
+  journal={IEEE Transactions on Instrumentation and Measurement},\
+  volume={71},\
+  pages={1--13},\
+  year={2022},\
+  publisher={IEEE}\
+}
+
+
---
+++ wse_update.py
@@ -0,0 +1,90 @@
+from torch.nn.modules.module import Module
+from torch.nn import _reduction as _Reduction
+import warnings
+import torch
+import numpy as np
+
+def low_freq_mutate_np( amp_src):
+    a_src = np.fft.fftshift(amp_src, axes=(-2, -1))
+    batch, _, h, w = a_src.shape
+    b = 3
+    c_h = np.floor(h/2.0).astype(int)
+    c_w = np.floor(w/2.0).astype(int)
+
+    h1 = c_h-b
+    h2 = c_h+b+1
+    w1 = c_w-b
+    w2 = c_w+b+1
+
+    mask = np.ones((batch, 1, h, w), np.uint8)
+    mask[:, :, h1:h2, w1:w2] = 0
+
+    mask_img_1 = a_src * mask + a_src
+    a_src = np.fft.ifftshift(mask_img_1, axes=(-2, -1))
+    return a_src
+
+
+def FA(src_img):
+    src_img_np = src_img.cuda().data.cpu().numpy()
+    fft_src_np = np.fft.fft2(src_img_np, axes=(-2, -1))
+    amp_src, pha_src = np.abs(fft_src_np), np.angle(fft_src_np)
+
+    amp_src_ = low_freq_mutate_np(amp_src)
+    fft_src_ = amp_src_ * np.exp(1j * pha_src)
+
+    src_in_trg = np.fft.ifft2(fft_src_, axes=(-2, -1))
+    src_in_trg = np.real(src_in_trg)
+    src_in_trg = src_in_trg.astype('float32')
+    src_in_trg = torch.from_numpy(src_in_trg)
+
+    return src_in_trg
+
+class _Loss(Module):
+    def __init__(self, size_average=None, reduce=None, reduction='mean'):
+        super(_Loss, self).__init__()
+        if size_average is not None or reduce is not None:
+            self.reduction = _Reduction.legacy_get_string(size_average, reduce)
+        else:
+            self.reduction = reduction
+
+class WSELoss(_Loss):
+    __constants__ = ['reduction']
+
+    def __init__(self, size_average=None, reduce=None, reduction='mean'):
+        super(WSELoss, self).__init__(size_average, reduce, reduction)
+
+    def forward(self, input_0, target_0, input_1, target_1):
+        ret = self.wse_loss(input_0, target_0, input_1, target_1)
+        return ret
+
+
+
+    def wse_loss(self, input, target, input_1, target_1, size_average=None, reduce=None, reduction='mean'):
+        if not (target.size() == input.size()):
+            warnings.warn("Using a target size ({}) that is different to the input size ({}). "
+                          "This will likely lead to incorrect results due to broadcasting. "
+                          "Please ensure they have the same size.".format(target.size(), input.size()),
+                          stacklevel=2)
+        enhance_target = FA(target)
+        enhance_target = enhance_target.cuda()
+        ret = (input - enhance_target) ** 2
+
+        enhance_target_1 = FA(target_1)
+        enhance_target_1 = enhance_target_1.cuda()
+        ret_1 = (input_1 - enhance_target_1) ** 2
+
+        mask_one = torch.cat((ret, ret_1), 2)
+
+        batch, c, h, w = mask_one.shape
+        mask_one = mask_one.view(batch, c, -1)
+        mask_one = torch.softmax(mask_one, dim=2)
+        mask_one = mask_one.view(batch, c, h, w)
+
+        mask_ret = mask_one[:, :, 0:256, :]
+        mask_ret1 = mask_one[:, :, 256:512, :]
+
+        ret = mask_ret * ret
+        ret_1 = mask_ret1 * ret_1
+        ret_out = torch.sum(ret) + torch.sum(ret_1)
+
+        return ret_out
